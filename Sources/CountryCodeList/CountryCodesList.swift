a9ae0e1908bdf0aaaccf686962ad2a8d80646c4f
Max Petrovsky <maksim.petrovsky@corewillsoft.com>
2022-03-15 17:58:16 +0100

    Adding Preorder posibilty

--- Sources/CountryCodeList/CountryCodesList.swift
+++ Sources/CountryCodeList/CountryCodesList.swift
@@ -5,10 +5,9 @@ public class CountryCodeList {
     case countryFileNotFound
   }
   
-//  private var list: [String: String]
   public private (set) var countries: [Country]
   
-  public init(locale: Locale = .autoupdatingCurrent) throws {
+  public init(locale: Locale = .autoupdatingCurrent, preorderCountries: [String] = []) throws {
     let decoder = JSONDecoder()
     let bundle: Bundle = Bundle.module
     guard let url = bundle.url(forResource: "countries", withExtension: "json") else {
@@ -16,11 +15,16 @@ public class CountryCodeList {
     }
     let data = try Data(contentsOf: url)
     let list = try decoder.decode([String: String].self, from: data)
-    countries = list
+    let countries = list
       .map { Country(phoneCode: $1,
                      code: $0,
                      name: locale.localizedString(forRegionCode: $0)!) }
-      .sorted { $0.name > $1.name }
+      .sorted { $0.name.compare($1.name, locale: locale) == .orderedAscending }
+    let preodred = preorderCountries.compactMap { (code) in
+      countries.first {$0.code == code }
+    }
+    self.countries = preodred
+    self.countries.append(contentsOf: countries.filter { !preorderCountries.contains($0.code) })
   }
   
   public convenience init(countryCode: String) throws {
